87d651307235c242f297abb7b95723f9f64bb8b4
Duvan Ramirez <solduvan11@gmail.com>
2024-04-01 14:54:31 -0500

    ciclos

---
+++ introPython2.py
@@ -0,0 +1,16 @@
+# while
+# while <condición_verdadera>:
+# expresiones booleanas(OR, AND) y operaciones de comparación (<, <=, >, >=, !=, ==)
+while 1==1:
+    print("ciclo")
+# para interumpir la ejecución d código se usa (CTRL + C)
+
+# Ciclos controlados por un contador (i, k)
+
+i=0
+k=0
+
+while i<10:
+    print("ciclo")
+    #Importante modificar el valor del contador
+    i+=1
